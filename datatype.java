3e0534f5dc19dbb5b0b0225b57932cd3d749677b
mrunmai <mrunmaikudale18@gmail.com>
2023-08-28 00:46:16 +0530

    primitive datatypes in java

---
+++ datatype.java
@@ -0,0 +1,24 @@
+public class datatype {
+    public static void main(String[] args) {
+        int num1 = 15632;
+        long num2 = 45436467644L; // suffix L is mandatory
+        short num3 = 2000;
+        byte num4 = 127; // range is -127 to 127
+
+        float num5 = 5.8f;
+        double num6 = 5.8;
+
+        char c = 'k';
+
+        boolean b = true;
+
+        System.out.println(num1);
+        System.out.println(num2);           
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
