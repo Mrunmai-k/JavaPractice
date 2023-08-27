1684276391733fd69e139dd63bf876b43ea120a4
mrunmai <mrunmaikudale18@gmail.com>
2023-08-28 00:51:41 +0530

    Literals in java about integer not string literals

---
+++ literals.java
@@ -0,0 +1,15 @@
+public class literals {
+    public static void main(String[] args) {
+        int num1 = 0x101;
+        int num2 = 0x7E;
+        int num3 = 10_00_00_000; // underscores are supported in between numbers to make it more readable 
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+
+        double num4 = 856263;
+        System.out.println(num4);
+        
+    }
+}
