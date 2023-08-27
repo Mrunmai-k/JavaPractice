42f3e07d0cd561c3c8162c4ee8c32e770542922f
mrunmai <mrunmaikudale18@gmail.com>
2023-08-27 23:46:16 +0530

    Variables in java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1 = 3;
+        int num2 = 6;
+        int result = num1 + num2 ;
+        System.out.println(num1 + num2);
+        System.out.println(result);
     }
 }
