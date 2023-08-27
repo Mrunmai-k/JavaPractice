8849036493b612b3d626e244874f8cf17bf7f531
mrunmai <mrunmaikudale18@gmail.com>
2023-08-28 00:13:35 +0530

    Variables in java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 class Hello {
     public static void main(String[] args) {
-        int num1 = 3;
-        int num2 = 6;
-        int result = num1 + num2 ;
-        System.out.println(num1 + num2);
-        System.out.println(result);
+        System.out.println("Hello World!");
     }
 }
---
+++ variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
+
+// strongly typed language = definedata type at the declaration, cannot change the data type once assigned
