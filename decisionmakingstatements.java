2e5f5bf4f10b855634d3c927f61ac723714b88f5
mrunmai <mrunmaikudale18@gmail.com>
2023-09-01 19:13:13 +0530

    Decision making statements in java

---
+++ decisionmakingstatements.java
@@ -0,0 +1,45 @@
+class statements {
+    public static void main(String[] args) {
+        // Decision making statements
+        // if statement
+        int x = 10;
+        int y = 12;
+        if (x + y > 20) {
+            System.out.println("x + y is greater than 20");
+        }
+
+        // if-else
+        if (x + y < 10) {
+            System.out.println("x + y is less than 10");
+        } else {
+            System.out.println("x + y is greater than 20");
+        }
+
+        // if-else-if ladder
+        String city = "Delhi";
+        if (city == "Meerut") {
+            System.out.println("city is meerut");
+        } else if (city == "Noida") {
+            System.out.println("city is noida");
+        } else if (city == "Agra") {
+            System.out.println("city is agra");
+        } else {
+            System.out.println(city);
+        }
+
+        // Nested if
+        String address = "Delhi, India";
+
+        if (address.endsWith("India")) {
+            if (address.contains("Meerut")) {
+                System.out.println("Your city is Meerut");
+            } else if (address.contains("Noida")) {
+                System.out.println("Your city is Noida");
+            } else {
+                System.out.println(address.split(",")[0]);
+            }
+        } else {
+            System.out.println("You are not living in India");
+        }
+    }
+}
