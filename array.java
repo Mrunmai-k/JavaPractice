0fa5777adbabfd4ef8126c8d59682694ded33b2b
mrunmai <mrunmaikudale18@gmail.com>
2023-09-05 12:55:58 +0530

    multi-dimensional arrays in java

--- array.java
+++ array.java
@@ -1,16 +1,49 @@
 public class array {
     public static void main(String[] args) {
-        int num[] = {4,7,3,2};
+        int num[] = { 4, 7, 3, 2 };
         num[1] = 6;
-        
-        System.out.println(num[2]);
+        int value = 1;
+        // System.out.println(num[2]);
 
         int dynamic[] = new int[4];
 
         for (int i = 0; i < dynamic.length; i++) {
-            System.out.println(dynamic[i]);    
+            dynamic[i] = value;
+            value++;
         }
-        
+        System.out.println("Dynamic Array");
+        for (int i = 0; i < dynamic.length; i++) {
+            System.out.print(dynamic[i] + " ");
+        }
+
+        System.out.println("\nMulti-dimensional Array");
+
+        // Multi-dimensional array
+
+        int nums[][] = new int[3][4];
+        // int rand = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                nums[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = 0; j < 4; j++) {
+        //         System.out.print(nums[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Optimized loop for array traversing
+        // foreach loop
+        for (int i[] : nums) {
+            for (int j : i) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
 
     }
 }
