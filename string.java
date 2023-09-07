884a79aa67cc57cb05fdac006f1f7d6a1c25b013
mrunmai <mrunmaikudale18@gmail.com>
2023-09-08 00:03:31 +0530

    StringBuffer and StringBuilder in java

--- string.java
+++ string.java
@@ -9,18 +9,35 @@ class StringDemo {
          * System.out.println(name.concat("Kudale"));
          */
 
-        String name = "Mrunmai";
-        System.out.println(name);
+        /*
+         * String name = "Mrunmai";
+         * System.out.println(name);
+         * 
+         * name = name + " Kudale"; // even if you change the name, this will create a
+         * new and point name to that object
+         * System.out.println("hello "+name);
+         * 
+         * String s1 = "mrun";
+         * String s2 = "mrun";
+         * 
+         * // This will not create two diff objects, this just creates two refs for one
+         * object;
+         * System.out.println(s1==s2); // the value for the addresses are same
+         * 
+         * // There is a string constant pool inside a heap which stores all the string
+         * constant
+         */
 
-        name = name + " Kudale";    // even if you change the name, this will create a new and point name to that object
-        System.out.println("hello "+name);
+         StringBuffer sb = new StringBuffer("Mrunmai");
+         System.out.println(sb.capacity());
+         System.out.println(sb.length());
 
-        String s1 = "mrun";
-        String s2 = "mrun";
+         sb.append("Kudale");
+         sb.deleteCharAt(2);
+         sb.insert(2,"u");
+         System.out.println(sb);
 
-        // This will not create two diff objects, this just creates two refs for one object;
-        System.out.println(s1==s2);         // the value for the addresses are same
-
-        // There is a string constant pool inside a heap which stores all the string constant
+        //  StringBuffer is thread safe and StringBuilder is not
+        
     }
 }
