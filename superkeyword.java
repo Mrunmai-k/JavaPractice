7c8f804cccbb2c908160ad9f438f20d7338044ee
mrunmai <mrunmaikudale18@gmail.com>
2023-09-11 10:54:45 +0530

    super() method in java

---
+++ superkeyword.java
@@ -0,0 +1,40 @@
+class A {
+    public A() {
+        super();
+        System.out.println("in A");
+    }
+
+    public A(int n) {
+        super();
+        System.out.println("in A int");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("in B");
+    }
+
+    public B(int n) {
+        super();
+        System.out.println("in B int");
+    }
+}
+
+public class superkeyword {
+    public static void main(String[] args) {
+        // B obj = new B();
+        B obj1 = new B(4);
+    }
+}
+
+/*
+ * By default, every constructor has super() which calls constructor of super
+ * class but if you want to pass a parameter, you have to explicitly call the
+ * super() method.
+ */
+/*
+ * By default, every class extends Object class which is not extended by any
+ * other class
+ */
