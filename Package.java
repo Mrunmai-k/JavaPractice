3b0497b1682b7062162da9c3bf2e18309810259d
mrunmai <mrunmaikudale18@gmail.com>
2023-09-11 23:50:09 +0530

    Access Specifiers in Java

--- Package.java
+++ Package.java
@@ -1,12 +1,26 @@
 import tools.*;
 
 class PackageDemo {
-    Calc obj1 = new Calc();
-    AdvCalc obj2 = new AdvCalc();
+    public static void main(String[] args) {
+        Calc obj1 = new Calc();
+        AdvCalc obj2 = new AdvCalc();
+
+        System.out.println(obj1.marks); // It says marks is not visible but it is there in Calc class, the problem is
+                                        // anything declared in a package is accessible only inside a package. If you
+                                        // want to access it outside the package then make it public.
+    }
 }
 
 /*
  * '*' is used for only files not for folders, means all the files will be
  * imported from tools folder but subfolder inside a tools folder will not be
  * imported.
+ */
+
+// Access specifiers in Java
+/*
+ * Public is accessible from anywhere
+ * Private is accessible in class itself 
+ * Default is accessible in the same package
+ * Protected is accessible in the same package and outside the package in the subclass 
  */
--- tools/Calc.java
+++ tools/Calc.java
@@ -1,6 +1,7 @@
 package tools;
 
 public class Calc {
+    public int marks = 80;
     public int add(int n1, int n2) {
         return n1 + n2;
     }
