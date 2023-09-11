fc44edd8cdc8d2b4a202bed65d06ce9b36d2d883
mrunmai <mrunmaikudale18@gmail.com>
2023-09-11 19:09:00 +0530

    single and multilevel inheritance in java

--- Inheritance.java
+++ Inheritance.java
@@ -18,6 +18,12 @@ class AdvCalc extends Calc{
     }
 }
 
+class VeryAdvCalc extends AdvCalc{
+    public double power(int n1,int n2){
+        return Math.pow(n1, n2);
+    }
+}
+
 public class Inheritance {
     public static void main(String[] args) {
         Calc obj = new Calc();
@@ -26,7 +32,20 @@ public class Inheritance {
 
         AdvCalc obj1 = new AdvCalc();
         System.out.println(obj1.add(r1, r2));  // 16 
+        System.out.println(obj1.sub(9, 4));  
         System.out.println(obj1.div(10, 5));    //2
+        System.out.println(obj1.mult(3,4));
+
+        VeryAdvCalc obj2 = new VeryAdvCalc();
+        System.out.println(obj2.add(r1, r2));  // 16 
+        System.out.println(obj2.sub(9, 4));  
+        System.out.println(obj2.div(10, 5));    //2
+        System.out.println(obj2.mult(3,4));
+
+        System.out.println(obj2.power(2, 5));
 
     }
 }
+
+// Calc -> AdvCalc This is called single level inheritance
+// Calc -> AdvCalc -> VeryAdvCalc This is called multilevel inheritance
