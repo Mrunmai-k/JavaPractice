7d0ac2c3233fd1bfc452a9055651a0a40a51508c
mrunmai <mrunmaikudale18@gmail.com>
2023-09-20 00:57:02 +0530

    try-finally block for closing the resources in java

--- Core Java Practice/UserInput.java
+++ Core Java Practice/UserInput.java
@@ -3,24 +3,39 @@ import java.util.Scanner;
 
 public class UserInput {
     public static void main(String[] args) throws IOException {
+
         System.out.println("Enter a number:");
-        // println() is the method of PrintStream class so to use it we need to create
-        // object of PrintStream class.
-        // But Java has already created a object named 'out' of PrintStream class in the
-        // System class. = out.println()
-        // As 'out' is static we need to call it using System class. = System.out
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num1 = Integer.parseInt(br.readLine());
-        System.out.println(num1);
+        BufferedReader br = null;
 
-        br.close();
+        try {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            int num1 = Integer.parseInt(br.readLine());
+            System.out.println(num1);
+        }
+
+        finally {
+            br.close();
+        }
 
         System.out.println("Enter a string: ");
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(str);
+        Scanner sc = null;
+        
+        try {
+            sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
 
-        sc.close();
+        finally {
+            sc.close();
+        }
+
     }
 }
+
+// println() is the method of PrintStream class so to use it we need to create
+// object of PrintStream class.
+// But Java has already created a object named 'out' of PrintStream class in the
+// System class. = out.println()
+// As 'out' is static we need to call it using System class. = System.out
